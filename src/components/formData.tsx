f1152c2d1313e502d514847a1edc22e2bf5f7560
Pe-te-r <mburupeter8526@gmail.com>
2024-06-19 15:29:39 +0300

    more styles

--- src/components/formData.tsx
+++ src/components/formData.tsx
@@ -2,7 +2,8 @@ import InputDiv  from "./input"
 import "./formdata.scss"
 
 const FormData=()=>{
-    const submited=()=>{
+    const submited=(e:any)=>{
+        e.preventDefault()
         alert("form submitted")
     }
 
@@ -10,7 +11,6 @@ const FormData=()=>{
         <>
             <h3 className="heading">Great to have you back</h3>
             <form className="form" onSubmit={submited}>
-                {/* <InputDiv tag ="name" type="text" /> */}
                 <InputDiv tag ="username" type="text" />
                 <InputDiv tag ="password" type="password" />
                 <button type="submit">submit</button>
